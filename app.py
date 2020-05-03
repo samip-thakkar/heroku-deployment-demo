4f70d1bbe2be0b616a6d90888304bcbb635385b5
Samip Thakkar <55044346+samip-thakkar@users.noreply.github.com>
2020-05-02 17:58:08 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, url_for, redirect, render_template, jsonify
+from pycaret.regression import *
+import pandas as pd
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+model = load_model('deployment_05022020')
+cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction = predict_model(model, data = data_unseen, round = 0)
+    prediction = int(prediction.Label[0])
+    return render_template('home.html', pred = 'Expected bill will be: {}'.format(prediction))
+
+@app.route('/predict_api', methods = ['POST'])
+def predict_api():
+    data = request.get_json(force = True)
+    data_unseen = pd.DataGrame([data])
+    prediction = perdict_model(model, data = data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
---
+++ main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Samip
+"""
+
+import pandas as pd
+data = pd.read_csv('insurance.csv')
+
+from pycaret.regression import *
+
+#Experiment 1
+s = setup(data, target = 'charges', session_id = 123)
+
+lr = create_model('lr')
+
+plot_model(lr)
+
+#Experiment 2
+s2 = setup(data, target = 'charges', session_id = 123, normalize = True, polynomial_features = True, trigonometry_features = True, feature_interaction = True,            bin_numeric_features= ['age', 'bmi'])
+lr = create_model('lr')
+
+plot_model(lr)
+
+save_model(lr, 'deployment_05022020')
+deployment_05022020 = load_model('deployment_05022020')
+
+"""
+import requests
+url = ''
+pred = requests.post(url, json = {'age':55, 'sex':'male', 'bmi':59, 'children':1, 'smoker':'male', 'region':'northwest'})
+print(pred.json())
+"""
